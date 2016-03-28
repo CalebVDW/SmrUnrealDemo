e0b0f89b3ee02579a82127cdf9adcd4415051788
CalebVDW <cxv9279@rit.edu>
2016-03-28 15:27:21 -0400

    Added branch for linking when building for Mac

--- Source/SMRDemo/SMRDemo.Build.cs
+++ Source/SMRDemo/SMRDemo.Build.cs
@@ -17,25 +17,41 @@ public class SMRDemo : ModuleRules
     public bool LoadSmr(TargetInfo Target)
     {
         bool isLibrarySupported = false;
-
+        string SmrRootDirectory = "C:/Git/SMRFork/";
         if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
         {
             isLibrarySupported = true;
-            //This hardcoded path obviously needs to be replaced with something that isn't so brittle
-            //The likely solution will be to copy the entire SMR directory into the Unreal plugins directory
-            //Then use a built in engine function to get the location of that directory
-            string SmrRootDirectory = "C:/Git/SMRFork/";
-            string LibraryDirectory = Path.Combine(SmrRootDirectory, "win32Build/lib/Release/");
-            Console.WriteLine(LibraryDirectory);
-            Console.WriteLine(Path.Combine(LibraryDirectory, "math.lib"));
+            string LibraryDirectory = Path.Combine(SmrRootDirectory, "macbuild/lib/Release/");
 
-            //Link static library
+            //Link static libraries
             PublicAdditionalLibraries.Add(Path.Combine(LibraryDirectory, "math.lib"));             
             PublicAdditionalLibraries.Add(Path.Combine(LibraryDirectory, "newmat.lib"));           
             PublicAdditionalLibraries.Add(Path.Combine(LibraryDirectory, "common.lib"));           
             PublicAdditionalLibraries.Add(Path.Combine(LibraryDirectory, "core.lib"));                    
             PublicAdditionalLibraries.Add(Path.Combine(LibraryDirectory, "log4cplus.lib"));
             PublicAdditionalLibraries.Add(Path.Combine(LibraryDirectory, "io.lib"));
+
+            //Add any necessary include directories
+            PublicIncludePaths.Add(Path.Combine(SmrRootDirectory, "Smr/SRC/common/"));
+            PublicIncludePaths.Add(Path.Combine(SmrRootDirectory, "Smr/SRC/SmrMath/"));
+            PublicIncludePaths.Add(Path.Combine(SmrRootDirectory, "Smr/SRC/SmrCore/"));
+            PublicIncludePaths.Add(Path.Combine(SmrRootDirectory, "Smr/SRC/SmrIO/"));
+            PublicIncludePaths.Add(Path.Combine(SmrRootDirectory, "Smr/SRC/SmrUtils/"));
+            PublicIncludePaths.Add(Path.Combine(SmrRootDirectory, "Smr/thirdParty/Universal/log4cplus-1.1.1-rc3/include/"));
+            PublicIncludePaths.Add(Path.Combine(SmrRootDirectory, "Smr/thirdParty/Universal/newmat/"));
+
+        }
+        else if(Target.Platform == UnrealTargetPlatform.Mac)
+        {
+            string LibraryDirectory = Path.Combine(SmrRootDirectory, "win32Build/lib/Release/");
+
+            //Link static library
+            PublicAdditionalLibraries.Add(Path.Combine(LibraryDirectory, "math.a"));
+            PublicAdditionalLibraries.Add(Path.Combine(LibraryDirectory, "newmat.a"));
+            PublicAdditionalLibraries.Add(Path.Combine(LibraryDirectory, "common.a"));
+            PublicAdditionalLibraries.Add(Path.Combine(LibraryDirectory, "core.a"));
+            PublicAdditionalLibraries.Add(Path.Combine(LibraryDirectory, "log4cplus.a"));
+            PublicAdditionalLibraries.Add(Path.Combine(LibraryDirectory, "io.a"));
 
             //Add any necessary include directories
             //Using all include directories from the CMake file for the UnrealAPI project
@@ -47,7 +63,6 @@ public class SMRDemo : ModuleRules
             PublicIncludePaths.Add(Path.Combine(SmrRootDirectory, "Smr/SRC/SmrUtils/"));
             PublicIncludePaths.Add(Path.Combine(SmrRootDirectory, "Smr/thirdParty/Universal/log4cplus-1.1.1-rc3/include/"));
             PublicIncludePaths.Add(Path.Combine(SmrRootDirectory, "Smr/thirdParty/Universal/newmat/"));
-
         }
         if (isLibrarySupported)
             Definitions.Add("UNREAL_SMR_BINDING");
